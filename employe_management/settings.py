66e4153615f50b56760ae62a444c318375dc8003
simanchal-12 <simanchal560@gmail.com>
2023-02-20 20:24:04 +0530

    vercel settings done

--- employe_management/settings.py
+++ employe_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l(pq8x-0$9tbgvmqi1o5!7t+5g^6tl(!shg)sw84q@p((ae#hl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 
@@ -120,8 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
